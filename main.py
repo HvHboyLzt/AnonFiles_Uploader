28a806b5890e92fb7c836eda2e0132a2c5133cab
HvHboy <54279945+HvHboyLzt@users.noreply.github.com>
2022-01-16 12:51:26 +0200

    Version 1.1

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from anonfile import AnonFile
 import os
 
+
 anon = AnonFile()
 files = os.listdir('./upload/')
 result = open('result.txt', 'w')
@@ -10,18 +11,8 @@ j = 0
 while j < s:
 	file = './upload/' + files[j]
 	upload = anon.upload(file, progressbar = False)
-	print(f'Uploaded: {upload.url.geturl()}')
+	print('\033[32m{}'.format("Uploaded: \033[0m") + upload.url.geturl())
 	j = j + 1
 	result.write(upload.url.geturl() + '\n')
 
-print('All files uploaded!\nCoded with love by HvHboy!')
-
-
-
-
-
-
-
-
-
-
+print('\033[1m{}'.format("All files uploaded!\nCoded with love by HvHboy!"))
